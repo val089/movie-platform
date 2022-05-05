32673cdf180f3767b56d1ff4d7d8990e7f0d9f25
Kamil Szerląg <kamilszerlag@gmail.com>
2022-05-05 17:35:17 +0200

    add player

--- src/App.tsx
+++ src/App.tsx
@@ -9,16 +9,15 @@ export const App = () => {
   return (
     <>
       <nav>
-        <Link to="/login">Login</Link>
         <Link to="/home">Home</Link>
-        <Link to="/player">Player</Link>
+        <Link to="/">Login</Link>
       </nav>
 
       <Routes>
-        <Route path="login" element={<LoginPage />} />
+        <Route path="/" element={<LoginPage />} />
         <Route element={<ProtectedRoute />}>
           <Route path="home" element={<HomePage />} />
-          <Route path="player" element={<PlayerPage />} />
+          <Route path="player/:movieId" element={<PlayerPage />} />
         </Route>
       </Routes>
     </>
